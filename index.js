c3a49873d737162498c4d7724ae3377087f9f6e0
Nexianz <38430931+Nexianz@users.noreply.github.com>
2018-04-16 16:28:35 +0200

    Update index.js

--- index.js
+++ index.js
@@ -69,7 +69,7 @@ bot.on("message", function(message) {
         var info = [
             "**SjokoBot 1.0**",
             "Programert og laget av **Nexianz#8554**",
-            "Versjon: 1.0test2"
+            "Versjon: 1.0test3"
         ]
         message.channel.send(info);
         break;
